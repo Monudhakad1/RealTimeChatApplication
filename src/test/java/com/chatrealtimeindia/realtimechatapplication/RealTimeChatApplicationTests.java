a42275011ed3d594703038d98e3bb95a9f8c9309
Monudhakad1 <monudhakad055@gmail.com>
2025-11-05 00:24:40 +0530

    Apply .gitignore correctly

---
+++ src/test/java/com/chatrealtimeindia/realtimechatapplication/RealTimeChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatrealtimeindia.realtimechatapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
